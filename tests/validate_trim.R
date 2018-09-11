b5d94d706fca356b33fc1a9d87888acfa60d157f
hb <hb@aroma-project.org>
2018-09-10 19:56:34 -0700

    TESTS: Add validation of output from step3 and step4

---
+++ tests/validate_trim.R
@@ -0,0 +1,32 @@
+path <- Sys.getenv("LG3_OUTPUT_PATH", "output")
+stopifnot(file_test("-d", path))
+
+truth <- list(
+  "Z00599-trim/Z00599-trim_R1.fastq.gz"            = list(size = 4541591250),
+  "Z00599-trim/Z00599-trim_R2.fastq.gz"            = list(size = 4787364118),
+  "Z00599-trim/Z00599-trim_R1.trimming_report.txt" = list(size = 3809 + c(-1, 1)*10),
+  "Z00599-trim/Z00599-trim_R2.trimming_report.txt" = list(size = 4011 + c(-1, 1)*10),
+  "Z00600-trim/Z00600-trim_R1.fastq.gz"            = list(size = 4860153452),
+  "Z00600-trim/Z00600-trim_R2.fastq.gz"            = list(size = 5029394683),
+  "Z00600-trim/Z00600-trim_R1.trimming_report.txt" = list(size = 3832 + c(-1, 1)*10),
+  "Z00600-trim/Z00600-trim_R2.trimming_report.txt" = list(size = 4044 + c(-1, 1)*10),
+  "Z00601-trim/Z00601-trim_R1.fastq.gz"            = list(size = 6021054501),
+  "Z00601-trim/Z00601-trim_R2.fastq.gz"            = list(size = 6312584266),
+  "Z00601-trim/Z00601-trim_R1.trimming_report.txt" = list(size = 3853 + c(-1, 1)*10),
+  "Z00601-trim/Z00601-trim_R2.trimming_report.txt" = list(size = 4047 + c(-1, 1)*10)
+)
+
+for (kk in seq_along(truth)) {
+	file <- names(truth)[kk]
+	pathname <- file.path(path, file)
+	size <- file.size(pathname)
+	cat(sprintf("- %s: %.0f bytes\n", pathname, size))
+	stopifnot(file_test("-f", pathname))
+	expected <- truth[[file]]$size
+	if (length(expected) == 1L) {
+	       stopifnot(size == expected)
+	} else {
+	       stopifnot(expected[1] <= size || size <= expected[2])
+	}
+	
+}
