f5ec7bf4d94c3e6d37d1bb6c932db78ac995df4b
Henrik Bengtsson <hb@aroma-project.org>
2018-09-18 10:40:24 -0700

    TROUBLESHOOTING: Adding more file-not-found checks to R scripts

--- scripts/MutDet_filter.R
+++ scripts/MutDet_filter.R
@@ -1,9 +1,18 @@
+assertFile <- function(pathname) {
+  if (!utils::file_test("-f", pathname)) {
+    pathnameX <- normalizePath(pathname, mustWork = FALSE)
+    stop(sprintf("File not found: %s => %s (current working directory is %s)", sQuote(pathname), sQuote(pathnameX), sQuote(getwd())))
+  }
+  invisible(pathname)
+}
+
 args <- commandArgs(trailingOnly = TRUE)
 
 infile <- args[1]
 outfile <- args[2]
 #stfa <- args[3]  ## sample_to_filter_against (ie Normal, or Primary if no Normal)
 
+assertFile(infile)
 data <- read.delim(infile, as.is=TRUE)
 
 alg <- which(data$algorithm == "SomaticIndelDetector")
