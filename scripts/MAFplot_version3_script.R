5d85d6ad7faf4fe26fed1be473e12d788bb0e63f
Henrik Bengtsson <hb@aroma-project.org>
2018-10-22 21:59:15 -0700

    R code style: styler::style_dir(scope = line_breaks) - guaranteed to preserve AST

--- scripts/MAFplot_version3_script.R
+++ scripts/MAFplot_version3_script.R
@@ -14,39 +14,39 @@ source(file.path(lg3_home, "scripts/MAFplot_version3.R"))
 dd <- loadMAF(pat)
 
 ##### modified by Ivan
-chrs=unique(dd[[pat]]$chromosome)
-chrs.num=length(chrs)
-   ## standardize chromosome notation (1-24, no "chr")
-   chrs=gsub("chr","",chrs)
-   chrs[chrs=="X"]=23
-   chrs[chrs=="Y"]=24
-   chrs=as.numeric(as.vector(chrs))
-cat("Found ",chrs.num,"chromosomes\n")
+chrs = unique(dd[[pat]]$chromosome)
+chrs.num = length(chrs)
+## standardize chromosome notation (1-24, no "chr")
+chrs = gsub("chr", "", chrs)
+chrs[chrs == "X"] = 23
+chrs[chrs == "Y"] = 24
+chrs = as.numeric(as.vector(chrs))
+cat("Found ", chrs.num, "chromosomes\n")
 print(chrs)
 
-if(chrs.num > 22) {
-	cat("Whole genome LOH plot\n")
-	png(paste0(pat,"_plots/",pat,".LOH.png"), width=10, height=4, units="in", res=300)
-	plotMAF(dd,conv,pat)
-	dev.off()
+if (chrs.num > 22) {
+  cat("Whole genome LOH plot\n")
+  png(paste0(pat, "_plots/", pat, ".LOH.png"), width = 10, height = 4, units = "in", res = 300)
+  plotMAF(dd, conv, pat)
+  dev.off()
 } else {
-	cat("Only ",chrs.num,"chromosomes available, skiping whole genome plot\n")
+  cat("Only ", chrs.num, "chromosomes available, skiping whole genome plot\n")
 }
 
-for(c in chrs) {
-  cat("LOH plot for chr",c,"\n")
-  pdf(paste0(pat,"_plots/",pat,".LOH.chr",c,".pdf"), width=10, height=4)
-  plotMAF(dd,conv,pat,ch=c)
+for (c in chrs) {
+  cat("LOH plot for chr", c, "\n")
+  pdf(paste0(pat, "_plots/", pat, ".LOH.chr", c, ".pdf"), width = 10, height = 4)
+  plotMAF(dd, conv, pat, ch = c)
   dev.off()
 }
 
-if(length(unique(dd[[pat]]$samp)) > 2) {
+if (length(unique(dd[[pat]]$samp)) > 2) {
   source(file.path(lg3_home, "scripts/MAFplot_version3_grid.R"))
   dd <- loadMAF(pat)
-  for(c in chrs) {
-	cat("Grid plot for chr",c,"\n")
-    pdf(paste0(pat,"_plots/",pat,".LOH.grid.chr",c,".pdf"), width=12, height=6)
-    plotMAF(dd,conv,pat,ch=c, grid=TRUE)
+  for (c in chrs) {
+    cat("Grid plot for chr", c, "\n")
+    pdf(paste0(pat, "_plots/", pat, ".LOH.grid.chr", c, ".pdf"), width = 12, height = 6)
+    plotMAF(dd, conv, pat, ch = c, grid = TRUE)
     dev.off()
   }
 }
