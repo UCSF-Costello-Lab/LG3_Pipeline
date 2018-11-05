0909cbd38c76516ff7a95fb1e0338a79eea8e466
hb <hb@aroma-project.org>
2018-11-05 08:46:11 -0800

    Undo 'readlink' mistake [#103]

--- scripts/mutdet_submit.sh
+++ scripts/mutdet_submit.sh
@@ -72,7 +72,6 @@ PBS=${LG3_HOME}/MutDet_TvsN.pbs
 assert_file_exists "${PBS}"
 
 WORKDIR=${LG3_OUTPUT_ROOT}/${PROJECT:?}/mutations/${PATIENT}_mutect
-WORKDIR=$(readlink -e "${WORKDIR}")
 mkdir -p "${WORKDIR}" || error "Can't create scratch directory ${WORKDIR}"
 
 XMX=Xmx8g
