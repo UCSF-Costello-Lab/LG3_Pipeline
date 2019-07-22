65bf48641046827b0e54106344366e67f9049c92
Henrik Bengtsson <hb@aroma-project.org>
2019-07-22 13:56:55 +0200

    ShellCheck: More fixed detected by ShellCheck 0.6.0

--- scripts/Germline.sh
+++ scripts/Germline.sh
@@ -84,7 +84,7 @@ echo "[Germline] Normal Sample: $normalname"
 echo "-------------------------------------------------"
 
 ## Construct string with one or more '-I <bam>' elements
-INPUTS=$(for i in ${bamdir}/*.bwa.realigned.rmDups.recal.bam
+INPUTS=$(for i in "${bamdir}"/*.bwa.realigned.rmDups.recal.bam
 do
         assert_file_exists "${i}"
         echo -n "-I $i "
@@ -184,7 +184,7 @@ else
    echo "[Germline] Found output ${PATIENT}.UG.snps.vcf -- Skipping..."
 fi
 
-for i in ${bamdir}/*.bam
+for i in "${bamdir}"/*.bam
 do
         tumorname=${i##*/}
         tumorname=${tumorname%%.bwa*}
