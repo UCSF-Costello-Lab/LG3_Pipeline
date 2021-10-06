130ec42a2323a78cce0c3336811969e326056b27
hb <hb@aroma-project.org>
2021-10-05 21:02:48 -0700

    Make sure MuTect jobs inherit all software env vars [#157]

--- scripts/mutdet_submit.sh
+++ scripts/mutdet_submit.sh
@@ -32,6 +32,7 @@ fi
 
 
 QSUB_ENVVARS="LG3_HOME=${LG3_HOME},LG3_INPUT_ROOT=${LG3_INPUT_ROOT},LG3_OUTPUT_ROOT=${LG3_OUTPUT_ROOT},EMAIL=${EMAIL}"
+QSUB_ENVVARS=$(lg3_qsub_envvar_append_software)
 QSUB_OPTS="${QSUB_OPTS} -d ${PWD:?}"
 
 ## Override the qsub email address?
